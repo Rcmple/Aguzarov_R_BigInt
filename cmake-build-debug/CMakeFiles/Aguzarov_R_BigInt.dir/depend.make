66c0639595a0eb7e226e9d039804f03d37b2786a
Rcmple <raaguzarov@edu.hse.ru>
2024-02-19 17:01:15 +0300

    removed cmake-build

--- cmake-build-debug/CMakeFiles/Aguzarov_R_BigInt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Aguzarov_R_BigInt.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/Aguzarov_R_BigInt.dir/tests/tests/tests.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Aguzarov_R_BigInt.dir/tests/tests/tests.cpp.o: \
- /mnt/c/Users/shayd/CLionProjects/Aguzarov_R_BigInt/tests/tests/tests.cpp \
- /usr/include/stdc-predef.h
